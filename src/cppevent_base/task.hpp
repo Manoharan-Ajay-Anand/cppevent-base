91e26e433612265559579138478fcba7956443e0
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-06-29 12:56:45 +0800

    void specialization for awaitable task

--- src/cppevent_base/task.hpp
+++ src/cppevent_base/task.hpp
@@ -82,6 +82,63 @@ struct awaitable_task {
     }
 };
 
+template<>
+struct awaitable_task<void> {
+    struct promise_type {
+
+        struct final_awaiter {
+            bool await_ready() noexcept { return false; }
+
+            std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> handle) {
+                auto& waiting = handle.promise().m_waiting;
+                if (waiting) {
+                    return waiting;
+                }
+                return std::noop_coroutine();
+            }
+
+            void await_resume() noexcept {}
+        };
+
+        std::coroutine_handle<> m_waiting;
+        std::exception_ptr m_exception;
+
+        awaitable_task<void> get_return_object() {
+            return { std::coroutine_handle<promise_type>::from_promise(*this) };
+        }
+
+        std::suspend_never initial_suspend() { return {}; }
+        final_awaiter final_suspend() noexcept { return {}; }
+        
+        void unhandled_exception() {
+            m_exception = std::current_exception();
+        }
+        
+        void return_void() {}
+    };
+
+    std::coroutine_handle<promise_type> m_handle;
+
+    ~awaitable_task() {
+        m_handle.destroy();
+    }
+
+    bool await_ready() noexcept {
+        return m_handle.done();
+    }
+    
+    void await_suspend(std::coroutine_handle<> handle) {
+        m_handle.promise().m_waiting = handle;
+    }
+    
+    void await_resume() {
+        promise_type& promise = m_handle.promise();
+        if (promise.m_exception) {
+            std::rethrow_exception(promise.m_exception);
+        }
+    }
+};
+
 }
 
 #endif
