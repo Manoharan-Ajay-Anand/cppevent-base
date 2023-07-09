5a0913a4ac9600177c5bd7a9dbeb1ef68d7a96d1
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-07-09 10:05:50 +0800

    event_bus test

---
+++ test/event_bus_test.cpp
@@ -0,0 +1,61 @@
+#include <doctest/doctest.h>
+
+#include <cppevent_base/event_bus.hpp>
+#include <cppevent_base/event_listener.hpp>
+
+class mock_listener : public cppevent::event_listener {
+private:
+    bool& m_read_event;
+    bool& m_write_event;
+public:
+    mock_listener(uint64_t id, bool& read_event, bool& write_event): cppevent::event_listener(id),
+                                                                     m_read_event(read_event),
+                                                                     m_write_event(write_event) {
+    }
+
+    void set_read_handler(const std::function<void()>& read_handler) {}
+    void set_write_handler(const std::function<void()>& write_handler) {}
+    
+    void on_event(bool can_read, bool can_write) {
+        m_read_event |= can_read;
+        m_write_event |= can_write;
+    }
+};
+
+TEST_CASE("event_bus test") {
+    bool read_event = false;
+    bool write_event = false;
+    cppevent::event_bus bus;
+
+    const cppevent::create_listener_func create_fn = [&read_event, &write_event](uint64_t id) {
+        return std::make_unique<mock_listener>(id, read_event, write_event);
+    };
+
+    SUBCASE("read only signal") {
+        auto* listener = bus.get_event_listener(create_fn);
+        bus.transmit_signal({ listener->get_id(), true, false });
+        CHECK(read_event);
+        CHECK_FALSE(write_event);
+    }
+
+    SUBCASE("write only signal") {
+        auto* listener = bus.get_event_listener(create_fn);
+        bus.transmit_signal({ listener->get_id(), false, true });
+        CHECK_FALSE(read_event);
+        CHECK(write_event);
+    }
+
+    SUBCASE("read and write signal") {
+        auto* listener = bus.get_event_listener(create_fn);
+        bus.transmit_signal({ listener->get_id(), true, true });
+        CHECK(read_event);
+        CHECK(write_event);
+    }
+
+    SUBCASE("no signal") {
+        auto* listener = bus.get_event_listener(create_fn);
+        bus.transmit_signal({ listener->get_id() * 10, true, true });
+        CHECK_FALSE(read_event);
+        CHECK_FALSE(write_event);
+    }
+}
