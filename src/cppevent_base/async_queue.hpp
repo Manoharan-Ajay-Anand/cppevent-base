9e26d37084f77f0055a5b9e3afc8640033f20d78
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-08-15 14:11:13 +0800

    update async_queue awaiter

--- src/cppevent_base/async_queue.hpp
+++ src/cppevent_base/async_queue.hpp
@@ -4,26 +4,49 @@
 #include "event_loop.hpp"
 #include "event_listener.hpp"
 #include "task.hpp"
-#include "async_signal.hpp"
 
 #include <queue>
 
 namespace cppevent {
 
 template <typename T>
-class async_queue {
+struct async_queue_awaiter {
 private:
-    async_signal m_signal;
-    std::queue<T> m_items;
+    std::queue<T>& m_items;
+    event_listener& m_listener;
 public:
-    async_queue(event_loop& loop): m_signal(loop) {
+    async_queue_awaiter(std::queue<T>& items, event_listener& listener): m_items(items),
+                                                                         m_listener(listener) {
     }
 
-    awaitable_task<int> await_items() {
-        if (m_items.empty()) {
-            co_await m_signal.await_signal();
-        }
-        co_return m_items.size();
+    bool await_ready() { return !m_items.empty(); }
+    
+    void await_suspend(std::coroutine_handle<> handle) {
+        m_listener.set_read_handler([handle]() {
+            handle.resume();
+        });
+    }
+    
+    long await_resume() { return m_items.size(); }
+};
+
+template <typename T>
+class async_queue {
+private:
+    std::queue<T> m_items;
+    event_loop& m_loop;
+    event_listener* m_listener;
+public:
+    async_queue(event_loop& loop): m_loop(loop) {
+        m_listener = loop.get_signal_listener();
+    }
+
+    ~async_queue() {
+        m_listener->detach();
+    }
+
+    async_queue_awaiter<T> await_items() {
+        return { m_items, *m_listener };
     }
 
     T& front() {
@@ -36,12 +59,12 @@ public:
 
     void push(const T& item) {
         m_items.push(item);
-        m_signal.get_trigger().activate();
+        m_loop.send_signal(m_listener->get_id(), true, false);
     }
 
     void push(T&& item) {
         m_items.push(std::move(item));
-        m_signal.get_trigger().activate();
+        m_loop.send_signal(m_listener->get_id(), true, false);
     }
 };
 
